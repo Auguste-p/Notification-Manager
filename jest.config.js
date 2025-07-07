bdb615751e9816ffd0aa054403e34d17a8338a50
Auguste-p <70906887+Auguste-p@users.noreply.github.com>
2025-07-07 12:10:09 +0200

    unit tests

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,27 @@
-const {createDefaultPreset} = require("ts-jest");
+const { createDefaultPreset } = require("ts-jest");
 
 const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-    testEnvironment: "node",
-    transform: {
-        ...tsJestTransformCfg,
-    },
-    testMatch: ["<rootDir>/test/**/*.spec.ts"]
+  testEnvironment: "node",
+  transform: {
+    ...tsJestTransformCfg,
+  },
+  testMatch: ["<rootDir>/test/**/*.spec.ts"],
 };
 
+collectCoverage = true;
+
+collectCoverageFrom = ["src/**/*.ts"];
+
 coverageThreshold = {
-    global: {
+  global: {
     branches: 50,
     functions: 50,
     lines: 50,
     statements: 50,
-    },
-}
+  },
+};
 
-coverageReporters = [
-    ["text", { file: "coverage.txt" }]
-]
+coverageReporters = [["text", { file: "coverage.txt" }]];
